e6f75cb716802ff205bb3840b062ea464b567bdb
Tuomas Hyötyniemi <bhq701@myy.haaga-helia.fi>
2024-09-29 15:32:35 +0300

    update & upgrade

--- main.js
+++ main.js
@@ -1,18 +1,1 @@
-let resizeTimeout;
-
-function updateViewportSize()
-{
-    clearTimeout(resizeTimeout);
-    resizeTimeout = setTimeout(() => {
-
-        const width = window.innerWidth;
-        const height = window.innerHeight;
-
-        document.getElementById('koko').innerHTML = `width: ${width}<br>height: ${height}`;
-
-    }, 200);
-
-    return;
-}
-
-window.addEventListener('resize', updateViewportSize);
+document.getElementById('width').innerHTML = window.innerWidth;
