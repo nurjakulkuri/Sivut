b19a6cfb1e55642b2582659b79bc2ab13ba31330
Tuomas Hyötyniemi <bhq701@myy.haaga-helia.fi>
2024-09-30 11:11:14 +0300

    hamburger update

--- main.js
+++ main.js
@@ -2,5 +2,6 @@ const nav_hamburger = document.getElementById('nav-hamburger');
 const nav_dropdown = document.getElementById('nav-dropdown');
 
 nav_hamburger.addEventListener('click', () => {
+    nav_hamburger.classList.toggle('active');
     nav_dropdown.classList.toggle('active');
 });
