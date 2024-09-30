22c15d8c296cc2ae6a8d9da4be95b8fa934099b9
Tuomas Hyötyniemi <bhq701@myy.haaga-helia.fi>
2024-09-30 12:52:34 +0300

    js update

--- main.js
+++ main.js
@@ -1,7 +1,28 @@
 const nav_hamburger = document.getElementById('nav-hamburger');
 const nav_dropdown = document.getElementById('nav-dropdown');
+const nav_overlay = document.getElementById('overlay');
 
-nav_hamburger.addEventListener('click', () => {
+nav_hamburger.addEventListener('click', (event) => {
+    event.stopImmediatePropagation();
     nav_hamburger.classList.toggle('active');
     nav_dropdown.classList.toggle('active');
+    nav_overlay.classList.toggle('active');
+    /*
+    nav_hamburger.classList.toggle('active');
+    nav_dropdown.classList.toggle('active');
+    nav_overlay.classList.toggle('active');
+    */
+});
+
+nav_overlay.addEventListener('click', () => {
+    nav_hamburger.classList.remove('active');
+    nav_dropdown.classList.remove('active');
+});
+
+document.addEventListener('click', (event) => {
+    if (!nav_hamburger.contains(event.target) && !nav_dropdown.contains(event.target)) {
+        nav_hamburger.classList.remove('active');
+        nav_dropdown.classList.remove('active');
+        nav_overlay.classList.remove('active');
+    }
 });
