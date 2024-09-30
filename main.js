3b913cdfadd0dc40bada8a1228159cf630f0305f
Tuomas Hyötyniemi <bhq701@myy.haaga-helia.fi>
2024-09-30 09:02:43 +0300

    restart, mobile first

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-document.getElementById('width').innerHTML = window.innerWidth;
+const nav_hamburger = document.getElementById('nav-hamburger');
+const nav_dropdown = document.getElementById('nav-dropdown');
+
+nav_hamburger.addEventListener('click', () => {
+    nav_dropdown.classList.toggle('active');
+});
